4d6145a24b256a0e1968c0f34744d84cc08de455
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 20:09:12 -0300

    Teste de uso do comentário de uma linha para deixar de executar uma linha de código em Java.

---
+++ src/TesteComentarioSimplesCodigo.java
@@ -0,0 +1,8 @@
+public class TesteComentarioSimplesCodigo {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Esse texto eu não quero que seja escrito.");
+        System.out.println("Esse texto eu quero que seja escrito.");
+    }
+}
